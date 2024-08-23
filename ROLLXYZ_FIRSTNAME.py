21cefa6b54eb411e559b583fbf142b3163d7d99f
Kislay Ranjan Nee Tandon <kislayranjanneetandon@Kislays-Laptop.local>
2024-08-23 13:39:38 +0530

    Better Question PDF

--- ROLLXYZ_FIRSTNAME.py
+++ ROLLXYZ_FIRSTNAME.py
@@ -1,5 +1,25 @@
 from SetCoveringProblemCreator import *
-        
+import random
+
+def initialize_population(size, subsets):
+    population = []
+    for _ in range(size):
+        individual = [random.choice([0, 1]) for _ in range(len(subsets))]
+        population.append(individual)
+    return population
+
+def geneticAlgorithm(subsets, population_size=100, generations=100, mutation_rate=0.01):
+    population = initialize_population(population_size, subsets)
+    for generation in range(generation):
+            
+
+
+
+
+    return bestIndividual
+
+
+
 def main():
     scp = SetCoveringProblemCreator()
 
@@ -11,7 +31,9 @@ def main():
     listOfSubsets = scp.ReadSetsFromJson("scp_test.json") #Your submission program should read from scp_test.json file and provide a good solution for the SetCoveringProblem.
     print(len(listOfSubsets))
     print()
-    
+
+    best_solution = geneticAlgorithm(listOfSubsets)
+    print("Best solution found:", best_solution)
 #    **********
 #    Write your code for find the solution to the SCP given in scp_test.json file using Genetic algorithm.
 
